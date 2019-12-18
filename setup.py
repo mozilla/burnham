ca99dcb2632595f69a2bef4f95edae24c5405cd9
Raphael Pierzina <raphael@hackebrot.de>
2019-12-18 22:05:05 +0100

    Bump version to 0.1.0.dev2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*args: str) -> str:
 
 setuptools.setup(
     name="burnham",
-    version="0.1.0.dev1",
+    version="0.1.0.dev2",
     author="Raphael Pierzina",
     author_email="raphael@hackebrot.de",
     maintainer="Raphael Pierzina",
--- src/burnham/__init__.py
+++ src/burnham/__init__.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-__version__ = "0.1.0.dev1"
+__version__ = "0.1.0.dev2"
 __title__ = "burnham"
 __description__ = "Application for end-to-end testing Mozilla's Glean telemetry. 👩‍🚀"
 __url__ = "https://github.com/hackebrot/burnham"
