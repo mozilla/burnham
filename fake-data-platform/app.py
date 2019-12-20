2abd1179e1a26e84d1c4e6dcf388f82dd62985f1
Raphael Pierzina <raphael@hackebrot.de>
2019-12-20 11:18:12 +0100

    Add heartbeat route and route for all other paths

--- fake-data-platform/app.py
+++ fake-data-platform/app.py
@@ -2,11 +2,23 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from flask import Flask
+# See https://flask.palletsprojects.com/en/1.1.x/patterns/singlepageapplications/
 
-app = Flask(__name__)
+import typing
+import flask
 
 
-@app.route("/")
-def hello() -> str:
-    return "hello"
+app = flask.Flask(__name__)
+
+
+@app.route("/heartbeat")
+def heartbeat() -> typing.Any:
+    """Return a status response."""
+    return flask.jsonify({"status": "healthy"})
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path: str) -> str:
+    """Return a response with the requested path."""
+    return f"path: '{path}'"
