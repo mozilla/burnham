ab01472802e0f44ff70c5104675bb68c152a452d
Raphael Pierzina <raphael@hackebrot.de>
2019-12-28 15:39:43 +0100

    Integrate Glean into CLI app

--- src/burnham/cli.py
+++ src/burnham/cli.py
@@ -5,8 +5,16 @@
 import urllib.request
 import click
 
+import glean
+import pkg_resources
+
+from . import __version__
+
 
 @click.command()
+@click.version_option(
+    __version__, "-V", "--version",
+)
 @click.option(
     "-p",
     "--platform",
@@ -15,9 +23,73 @@ import click
     required=True,
     envvar="BURNHAM_PLATFORM_URL",
 )
-@click.argument("path")
-def burnham(platform: str, path: str) -> None:
+@click.option(
+    "-t",
+    "--telemetry",
+    help="Enable telemetry recording and submission",
+    type=bool,
+    default=False,
+    is_flag=True,
+    envvar="BURNHAM_TELEMETRY",
+)
+@click.option(
+    "-r",
+    "--test-run",
+    help="ID of the current test run",
+    type=str,
+    required=True,
+    envvar="BURNHAM_TEST_RUN",
+)
+@click.option(
+    "-n",
+    "--test-name",
+    help="Name of the current test",
+    type=str,
+    required=True,
+    envvar="BURNHAM_TEST_NAME",
+)
+@click.option(
+    "-v",
+    "--verbose",
+    help="Print debug information to the console",
+    type=bool,
+    default=False,
+    is_flag=True,
+    envvar="BURNHAM_VERBOSE",
+)
+def burnham(
+    platform: str, telemetry: bool, test_run: str, test_name: str, verbose: bool
+) -> None:
     """Send a GET request to the platform and print the response."""
 
-    with urllib.request.urlopen(f"{platform}/{path}") as response:
-        click.echo(response.read())
+    click.echo(f"platform: {platform}")
+    click.echo(f"telemetry: {telemetry}")
+    click.echo(f"test_run: {test_run}")
+    click.echo(f"test_name: {test_name}")
+    click.echo(f"verbose: {verbose}")
+
+    metrics = glean.load_metrics(
+        pkg_resources.resource_filename(__name__, "config/metrics.yaml")
+    )
+    pings = glean.load_pings(
+        pkg_resources.resource_filename(__name__, "config/pings.yaml")
+    )
+
+    from glean.config import Configuration
+    from glean import Glean
+
+    Glean.set_upload_enabled(telemetry)
+    Glean.initialize(
+        application_id="burnham",
+        application_version=__version__,
+        configuration=Configuration(server_endpoint=platform, log_pings=verbose,),
+    )
+
+    metrics.test.burnham.test_run.set(test_run)
+    metrics.test.burnham.test_name.set(test_name)
+    pings.test_start.send()
+
+    metrics.test.burnham.space_travel["warp_drive"].add(4)
+    pings.discovery.send()
+
+    pings.test_finish.send()
