09bf97c316772f2639aabd3373c430750bc938e9
Raphael Pierzina <raphael@hackebrot.de>
2020-06-29 11:11:55 +0200

    Create QueryJobConfig for burnham_test_run

--- bigquery/tests/test_burnham.py
+++ bigquery/tests/test_burnham.py
@@ -4,13 +4,16 @@
 
 from typing import Any, List
 
-from google.cloud.bigquery import Client
+from google.cloud import bigquery
 
 
-def test_burnham(bq_client: Client, query: str, want: List[Any]):
+def test_burnham(
+    client: bigquery.Client,
+    query_job_config: bigquery.QueryJobConfig,
+    query: str,
+    want: List[Any],
+):
     """Test that the Glean telemetry in BigQuery matches what we expect."""
-
-    job = bq_client.query(query)
-    got = [row for row in job.result()]
-
+    query_job = client.query(query, job_config=query_job_config)
+    got = [row for row in query_job.result()]
     assert got == want
