70815ed2bc2cbd700750721058176acba0e1bee5
Raphael Pierzina <raphael@hackebrot.de>
2020-08-10 17:00:38 +0200

    Define dataclass represeting a BigQuery table row

--- bigquery/tests/plugins/store_results_in_bigquery.py
+++ bigquery/tests/plugins/store_results_in_bigquery.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from dataclasses import dataclass
+
 
 def pytest_addoption(parser):
     """Hook implementation that adds a "--results-table" CLI option."""
@@ -16,6 +18,29 @@ def pytest_addoption(parser):
         type=str,
         required=False,
     )
+
+
+@dataclass
+class TableRow:
+    """Dataclass representing a row in the results table.
+
+    See https://github.com/mozilla/bigquery-etl/blob/master/sql/burnham_derived/test_results_v1/schema.bq.json
+    """
+
+    # Time at which this test report was submitted
+    submission_timestamp: str
+    # ID of the test run
+    test_run: str
+    # Name of the test scenario
+    test_name: str
+    # Outcome of the test scenario
+    test_outcome: str
+    # Duration of the test scenario
+    test_duration_millis: int
+    # URL for viewing logs of this task instance in Airflow
+    test_log_url: str
+    # Markdown-formatted report for error or failed tests
+    test_report: str
 
 
 class StoreResults:
